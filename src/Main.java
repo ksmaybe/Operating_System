3337a360fa3ddfe7ef761a1a6a260cee69b9b66f
ksmaybe <ksmaybe@gmail.com>
2019-09-20 21:17:48 -0400

    whatever

--- src/Main.java
+++ src/Main.java
@@ -5,81 +5,141 @@ public class Main {
     public static boolean isNumeric(String str){ return str.chars().allMatch(Character::isDigit);}
 
 
+
     public static void main(String[] args) throws Exception {
         boolean keyboard=false;
+        Integer max_limit_character=8;
         if (args.length!=1) keyboard=true;
         String[] error=new String[]{
-                "Warning: %s is defined but not used.",
-                "Error: %s is multiply defined. First definition used.",
-                "Error: %s is not defined. Given value zero.",
-                "Error: Multiple symbols listed. All but last usage ignored.",
-                "Error: Address exceed size of module. Address treated as zero.",
-                "Error: Immediate address in use list. Treated as external.",
-                "Error: External address not in use list. Treated as immediate address.",
-                "Error: Absolute address exceeds size of machine. Largest legal value used."
+                "Warning: %s is defined but not used.", //0
+                "Error: %s is multiply defined. First definition used.", //1
+                "Error: %s is not defined. Given value zero.", //2
+                "Error: Multiple symbols listed. All but last usage ignored.", //3
+                "Error: Address exceed size of module. Address treated as zero.", //4
+                "Error: Immediate address in use list. Treated as external.", //5
+                "Error: External address not in use list. Treated as immediate address.", //6
+                "Error: Absolute address exceeds size of machine. Largest legal value used." //7
         };
-        if (!keyboard){}
+        if (!keyboard){
+
+        }
         else{
 
             //File file=new File(args[0]);
-            File file=new File("input-1");
-            Scanner first = new Scanner(file);
+            File file=new File("input-3");
+            Scanner input = new Scanner(file);
             int mod_address=0;
-            String s="";   //symbol scanned
-            int totalMods=first.nextInt();
-            int numSymbols=0;
-            int defSymbol=0;
+            String s="";   //string scanned
+            int totalMods=input.nextInt(); //Number of modules
+            int NT=0; //Number of times used
+            int num=0;  //integer scanned
             HashMap<String,Integer> defList=new HashMap<String, Integer>();
-            int totalAddresses=0;
-            int totalUses=0;
+            int sizeMachine=0;
             HashMap<String,Integer> symbolUsed= new HashMap<String, Integer>();
-            HashMap<String,List<Integer>> useList = new HashMap<String,List<Integer>>();
+            HashMap<Integer,String> useList = new HashMap<Integer,String>();
+            HashMap<Integer,Integer> moduleSize=new HashMap<Integer,Integer>();
+            HashMap<String,Integer> location=new HashMap<String,Integer>();
+            ArrayList<String> definition_error= new ArrayList<String>();
             //first pass
             for (int i=0;i<totalMods;i++) {
                 //definition list
-                numSymbols = first.nextInt();
-                for (int k = 0; k < numSymbols; k++) {
-                    s = first.next();
-                    defSymbol=first.nextInt();
+
+                NT = input.nextInt();
+                for (int k = 0; k < NT; k++) {
+                    s = input.next();
+                    num=input.nextInt();
+
                     if(defList.containsKey(s)){
-                        defList.put(s,(defSymbol+mod_address));
-                        System.out.println(error[0]);
+                        System.out.println(String.format(error[1],s));
                     } else{
-                        defList.put(s,defSymbol+mod_address);
-                        symbolUsed.put(s,i);
+                        defList.put(s,num+mod_address);
                     }
-                    defSymbol = first.nextInt();
-                    System.out.print(s);
-                    System.out.print("  ");
-                    System.out.println(defSymbol);
+                    location.put(s,i);
 
                     }
                 //use list
-                numSymbols = first.nextInt();
-                for (int k = 0; k < numSymbols; k++) {
-                    s = first.next();
-                    defSymbol=first.nextInt();
+                NT = input.nextInt();
+
+                for (int k = 0; k < NT; k++) {
+                    s = input.next();
+                    num=input.nextInt();
                     if(defList.containsKey(s)){
-                        defList.put(s,(defSymbol+mod_address));
-                        System.out.println(error[0]);
+                        symbolUsed.put(s,(num+mod_address));
+                        useList.put(num+mod_address,s);
+
                     } else{
-                        defList.put(s,defSymbol+mod_address);
-                        symbolUsed.put(s,i);
+                        symbolUsed.put(s,mod_address);
+                        useList.put(mod_address,s);
                     }
-                    defSymbol = first.nextInt();
-                    System.out.print(s);
-                    System.out.print("  ");
-                    System.out.println(defSymbol);
+
 
                 }
                 //program text
-                numSymbols=first.nextInt();
-                for(int k =0;k<numSymbols;k++){
-                    defSymbol=first.nextInt();
-                    System.out.println(defSymbol);
+                NT=input.nextInt();
+                for(int k =0;k<NT;k++){
+                    num=input.nextInt();
+                    moduleSize.put(i,mod_address+NT);
+                }
+                mod_address+=NT;
+            }// end of first pass
+            sizeMachine=mod_address;
+            System.out.println("Symbol Table");
+            //check if symbol defined but not used
+            for(String key: defList.keySet()){
+                if(!symbolUsed.containsKey(key)) definition_error.add(String.format(error[0],key));
+                //checks if address exceeds module size, if so becomes 0/relative
+                if(defList.get(key)>moduleSize.get(location.get(key))){
+                    defList.put(key,moduleSize.get(location.get(key)));
+                    System.out.println(String.format(error[4],s));
+                }
+                System.out.println(key+"="+defList.get(key));
+            }
+            //check if symbol used but not defined
+            for(String key: symbolUsed.keySet()){
+                if(!defList.containsKey(key)) System.out.println(String.format(error[2],key));
+            }
+            //start second pass
+            input=new Scanner(file);
+            System.out.println("Memory Map");
+            mod_address=0;
+            totalMods=input.nextInt();
+            for (int i=0;i<totalMods;i++) {
+                //definition list
+                NT = input.nextInt();
+                for (int k = 0; k < NT; k++) {
+                    input.next();
+                    input.nextInt();
+                }
+                //use list
+                NT = input.nextInt();
+                for (int k = 0; k < NT; k++) {
+                    input.next();
+                    input.nextInt();
+                }
+                //program text
+                NT=input.nextInt();
+                for(int k =0;k<NT;k++){
+                    Integer val=input.nextInt();
+                    Integer type=val%10;
+                    val=val/10;
+                    Integer op=val/1000;
+                    Integer v=0;
+                    if(type==1 || type==2){
+                        v=val;
+                    }else if(type==3){
+                        v=val+mod_address;
+                    }else if(type==4){
+                        v=(op*1000);//+defList.get(useList.get(mod_address+k));
+                    }
+                    System.out.println((mod_address+k)+":   "+v);
 
                 }
+                mod_address+=NT;
+            }// end of second pass
+            for(int i=0;i<definition_error.size();i++){
+                System.out.println(definition_error.get(i));
             }
-            System.out.println("Hello World!");}
+
+        }
     }
 }
---
+++ src/lala.java
@@ -0,0 +1,9 @@
+
+
+public class lala {
+    public static void main(String[] args) throws Exception {
+        if (false) System.out.println("lala");
+        else System.out.println("nono");
+
+    }
+}
