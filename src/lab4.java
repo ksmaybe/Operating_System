03f5993181d878e08af4b126bb9cb7dc1e64b8fd
ksmaybe <ksmaybe@gmail.com>
2019-12-04 13:07:27 -0500

    going lab 4

--- src/lab4.java
+++ src/lab4.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 
 public class lab4{
+    int q=3; //quantum size
     String randomFileName = "random-numbers.txt";
     File randomFile = new File(randomFileName);
     Scanner random= new Scanner(randomFile);
@@ -15,6 +16,7 @@ public class lab4{
     public void lifo(){
         Stack s= new Stack();
 
+        random.nextInt();
     }
     public void wp1(int S) {
 
@@ -28,19 +30,19 @@ public class lab4{
 
     public void main(String[] args) throws Exception {
         //System.out.println(args.length);0
-        int M = Integer.getInteger(args[2]);
-        int P = Integer.getInteger(args[3]);
-        int S = Integer.getInteger(args[4]);
-        int J = Integer.getInteger(args[5]);
-        int N = Integer.getInteger(args[6]);
-        String alg = args[7];
+        int M = Integer.getInteger(args[2]);    //Machine size in words
+        int P = Integer.getInteger(args[3]);    //Page size in words
+        int S = Integer.getInteger(args[4]);    //Size of each process
+        int J = Integer.getInteger(args[5]);    //Job mix, determines A,B,C
+        int N = Integer.getInteger(args[6]);    //Number of references for each process
+        String R = args[7];                     //Replacement algorithm, LIFO, RANDOM, or LRU
 
         System.out.println("The machine size is "+M);
         System.out.println("The page size is "+P);
         System.out.println("The process size is "+S);
         System.out.println("The job mix number is "+J);
         System.out.println("The number of references per process is "+N);
-        System.out.println("The replacement algorithm is "+alg);
+        System.out.println("The replacement algorithm is "+R);
         System.out.println();
     }
 }
